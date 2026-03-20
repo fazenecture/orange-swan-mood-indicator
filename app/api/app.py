1bd5657c7e39b41697e5ef99d426c0c281bc7bea
fazenecture <vivekkumarsingh2002@gmail.com>
2026-03-21 00:25:34 +0530

    feat: added history & market overview

--- app/api/app.py
+++ app/api/app.py
@@ -9,6 +9,7 @@ api.add_middleware(
     allow_origins=[
         "https://orange-swan-index.lovable.app",
         "https://id-preview--eae7594e-2204-4532-b499-d86c4c79210e.lovable.app",
+        "https://orange-swan-index-web.vercel.app/"
     ],
     allow_methods=["GET", "OPTIONS"],
     allow_headers=["*"],
